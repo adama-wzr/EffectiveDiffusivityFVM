174df75df966a535647ecf68f73f22092bd7b2e8
adama-wzr <adama@ku.edu>
2025-01-17 16:02:19 -0500

    Flood-Fill
    - Flood fill, simple BC's, and diffusion coefficients have been successfully implemented.
    - C++17 is required for this code to work

--- src_GPU/helper.cuh
+++ src_GPU/helper.cuh
@@ -89,6 +89,10 @@ typedef struct
     double dz;
 } meshInfo;
 
+// Define coords for Flood Fill
+
+typedef std::tuple<int, int, int> coord;
+
 
 int printOptions(options* opts)
 {
@@ -449,7 +453,7 @@ int SetDC3D(options* opts, meshInfo* mesh, double* DC, char* simObject)
             - None.
         The function will set the diffusion coefficient of the grid. 
     */
-    
+
     for(int k = 0; k<mesh->numCellsZ; k++)
     {
         for(int i = 0; i<mesh->numCellsY; i++)
@@ -475,6 +479,302 @@ int SetDC3D(options* opts, meshInfo* mesh, double* DC, char* simObject)
     }
 
     return 0;
+}
+
+int SetBC_DeffSetup3D(options* opts, meshInfo* mesh, char* BC, double* BC_Value)
+{
+    /*
+        Function SetBC_DeffSetup:
+        Inputs:
+            - pointer to options struct
+            - pointer to mesh struct
+            - pointer to BC classification array
+            - pointer to BC_value array (value of BC for Neumann or Dirichlet)
+        Output:
+            - None
+        
+        The function will classify the entire BC array with 2 Dirichlet conditions on the left
+        and right, while all other boundaries will be set to zero flux Neumann boundaries.
+    */
+    // Set some variables to help
+    int nCols, nRows, nSlices;
+    nCols = mesh->numCellsX + 2;
+    nRows = mesh->numCellsY + 2;
+    nSlices = mesh->numCellsZ + 2;
+    // On the BC array, we need to classify right and left as Dirichlet, and assign the values
+    // on BC_Value
+    // All the Neumann condition have to be assigned on BC, but no change in BC_Value is required.
+
+    int right, left, top, bottom, front, back;
+
+    left = 0;
+    right = mesh->numCellsX + 1;
+
+    top = 0;
+    bottom = mesh->numCellsY + 1;
+
+    front = 0;
+    back = mesh->numCellsZ + 1;
+
+    // right and left boundaries (Dirichlet)
+
+    for(int row = 0; row < nRows; row++)
+    {
+        for(int slice = 0; slice < nSlices; slice++)
+        {
+            long int index1 = slice*nRows*nCols + row*nRows + left;
+            long int index2 = slice*nRows*nCols + row*nRows + right;
+            // Left boundary
+            BC[index1] = 1;         // Dirichlet flag
+            BC_Value[index1] = opts->CLeft;
+            // Rigth boundary
+            BC[index2] = 1;
+            BC_Value[index2] = opts->CRight;
+        }
+    }
+
+    // Top and Bottom boundaries (Neumann)
+
+    for(int slice = 0; slice < nSlices; slice++)
+    {
+        for(int col = 0; col < nCols; col++)
+        {
+            long int index1 = slice*nRows*nCols + top*nCols + col;
+            long int index2 = slice*nRows*nCols + bottom*nCols + col;
+            // Top
+            BC[index1] = 2;
+            // Bottom
+            BC[index2] = 2;
+        }
+    }
+
+    // Back and Front boundaries (Neumann)
+
+    for(int row = 0; row < nRows; row++)
+    {
+        for(int col = 0; col < nCols; col++)
+        {
+            int index1 = front*nRows*nCols + row*nCols + col;
+            int index2 = back*nRows*nCols + row*nCols + col;
+            // Front
+            BC[index1] = 2;
+            // Back
+            BC[index2] = 2;
+        }
+    }
+
+    return 0;
+}
+
+int FloodFill3D_DeffSetup(meshInfo* mesh, char* BC, double* DC)
+{
+    /*
+        FloddFill3D_DeffSetup function:
+        Inputs:
+            - pointer to mesh struct
+            - pointer to array with BC's
+            - pointer to array with DC's
+        Outputs:
+            - None
+        
+        The function will search the domain, and will set all DC values that are too
+        low to a Neumann BC with zero flux. Non-participating media will also be flagged
+        accordingly.
+    */
+
+    char* Domain = (char *)malloc(mesh->nElements*sizeof(char));
+
+    // Initialize all the impermeable matter in the domain:
+
+    for(long int index = 0; index < mesh->nElements; index++)
+    {
+        int slice = index/(mesh->numCellsX*mesh->numCellsY);
+        int row = (index - slice*mesh->numCellsX*mesh->numCellsY);
+        int col = (index - slice*mesh->numCellsX*mesh->numCellsY - row*mesh->numCellsX);
+        int indexBC = (slice + 1)*mesh->numCellsX*mesh->numCellsY + (row + 1)*mesh->numCellsX + (col + 1);
+        if(DC[index] < 1e-15)
+        {
+            Domain[index] = 1;
+            BC[indexBC] = 2;    // set BC to Neumann
+        } else
+        {
+            Domain[index] = -1;
+        }
+    }
+
+    // Find Fluid in both boundaries, add to open list
+
+    std::set<coord> cList;
+
+    int left = 0;
+    int right = mesh->numCellsX;
+
+    for(int row = 0; row < mesh->numCellsY; row++)
+    {
+        for(int slice = 0; slice < mesh->numCellsZ; slice++)
+        {
+            long int indexL = slice*mesh->numCellsX*mesh->numCellsY + row*mesh->numCellsX + left;
+            long int indexR = slice*mesh->numCellsX*mesh->numCellsY + row*mesh->numCellsX + right;
+            // set left
+            if(Domain[indexL] == -1)
+            {
+                Domain[indexL] = 0;
+                cList.insert(std::tuple(left, row, slice));
+            }
+            if(Domain[indexR] == -1)
+            {
+                Domain[indexR] = 0;
+                cList.insert(std::tuple(right, row, slice));
+            }
+        }
+    }
+
+    // Search Full Domain
+
+    while(!cList.empty())
+    {
+        // pop first item on the list
+        coord pop = *cList.begin();
+
+        // remove from open list
+        cList.erase(cList.begin());
+
+        // get coordinates from the list
+        int col     = std::get<0>(pop);
+        int row     = std::get<1>(pop);
+        int slice   = std::get<2>(pop);
+
+        /*
+            We need to check North, South, East, and West for more fluid:
+            
+            North = col + 0, row - 1, slice + 0
+            South = col + 0, row + 1, slice + 0
+            East  = col + 1, row + 0, slice + 0
+            West  = col - 1, row + 0, slice + 0
+            Front = col + 0, row + 0, slice - 1
+            Back  = col + 0, row + 0, slice + 1
+        
+            Note that diagonals are not considered a connection.
+            This code assumes no periodic boundary conditions (currently).
+        */
+
+        int tempRow, tempCol, tempSlice;
+        long int tempIndex;
+        
+        // North
+
+        tempCol = col;
+        tempSlice = slice;
+
+        if(row != 0)
+        {
+            tempRow = row - 1;
+            tempIndex = tempSlice*mesh->numCellsX*mesh->numCellsY 
+                        + tempRow*mesh->numCellsX + tempCol;
+            if (Domain[tempIndex] == -1)
+            {
+                Domain[tempIndex] = 0;
+                cList.insert(std::tuple(tempCol, tempRow, tempSlice));
+            }
+        }
+
+        // South
+
+        if(row != mesh->numCellsY - 1)
+        {
+            tempRow = row + 1;
+            tempIndex = tempSlice*mesh->numCellsX*mesh->numCellsY 
+                        + tempRow*mesh->numCellsX + tempCol;
+            if (Domain[tempIndex] == -1)
+            {
+                Domain[tempIndex] = 0;
+                cList.insert(std::tuple(tempCol, tempRow, tempSlice));
+            }
+        }
+
+        // Front
+
+        tempCol = col;
+        tempRow = row;
+
+        if(slice != 0)
+        {
+            tempSlice = slice - 1;
+            tempIndex = tempSlice*mesh->numCellsX*mesh->numCellsY 
+                        + tempRow*mesh->numCellsX + tempCol;
+            if (Domain[tempIndex] == -1)
+            {
+                Domain[tempIndex] = 0;
+                cList.insert(std::tuple(tempCol, tempRow, tempSlice));
+            }
+        }
+
+        // Back
+
+        if(slice != mesh->numCellsZ - 1)
+        {
+            tempSlice = slice + 1;
+            tempIndex = tempSlice*mesh->numCellsX*mesh->numCellsY 
+                        + tempRow*mesh->numCellsX + tempCol;
+            if (Domain[tempIndex] == -1)
+            {
+                Domain[tempIndex] = 0;
+                cList.insert(std::tuple(tempCol, tempRow, tempSlice));
+            }
+        }
+
+        // West
+
+        tempRow = row;
+        tempSlice = slice;
+
+        if(col != 0)
+        {
+            tempCol = col - 1;
+            tempIndex = tempSlice*mesh->numCellsX*mesh->numCellsY 
+                        + tempRow*mesh->numCellsX + tempCol;
+            if (Domain[tempIndex] == -1)
+            {
+                Domain[tempIndex] = 0;
+                cList.insert(std::tuple(tempCol, tempRow, tempSlice));
+            }
+        }
+
+        // East
+
+        if(col != mesh->numCellsX - 1)
+        {
+            tempCol = col + 1;
+            tempIndex = tempSlice*mesh->numCellsX*mesh->numCellsY 
+                        + tempRow*mesh->numCellsX + tempCol;
+            if (Domain[tempIndex] == -1)
+            {
+                Domain[tempIndex] = 0;
+                cList.insert(std::tuple(tempCol, tempRow, tempSlice));
+            }
+        }
+        // repeat until cList is empty
+    }
+
+    // Every flag that is still -1 means a non-participating media
+
+    for(int index = 0; index<mesh->nElements; index++)
+    {
+        if(Domain[index] != -1) continue;
+
+        int slice = index/(mesh->numCellsX*mesh->numCellsY);
+        int row = (index - slice*mesh->numCellsX*mesh->numCellsY);
+        int col = (index - slice*mesh->numCellsX*mesh->numCellsY - row*mesh->numCellsX);
+        int indexBC = (slice + 1)*mesh->numCellsX*mesh->numCellsY + (row + 1)*mesh->numCellsX + (col + 1);
+
+        BC[indexBC] = -1;
+    }
+    
+    // memory management
+    free(Domain);
+
+    return 0;
+
 }
 
 int SteadyStateSim3D(options* opts)
@@ -531,7 +831,32 @@ int SteadyStateSim3D(options* opts)
 
     // note BC array has space for ``ghost'' grid boundaries
 
+    // Set DC's
+
     SetDC3D(opts, &mesh, DC, simObject);
+
+    // Set BC's
+
+    SetBC_DeffSetup3D(opts, &mesh, BC, BC_Value);
+    
+    // set if D[i,,j,k] < 10^-15, D[i,j,k] = 0 becomes a Neumann BC
+
+    for(int index = 0; index<mesh.nElements; index++)
+    {
+        int slice = index/(mesh.numCellsX*mesh.numCellsY);
+        int row = (index - slice*mesh.numCellsX*mesh.numCellsY);
+        int col = (index - slice*mesh.numCellsX*mesh.numCellsY - row*mesh.numCellsX);
+        int indexBC = (slice + 1)*mesh.numCellsX*mesh.numCellsY + (row + 1)*mesh.numCellsX + (col + 1);
+        if(DC[index] < 1e-15)
+        {
+            DC[index] = 0;
+            BC[indexBC] = 2;    // set Neumann BC with zero flux
+        }
+    }
+
+    // If any phase is impermeable, need to find all participating media
+
+    FloodFill3D_DeffSetup(&mesh, BC, DC);
 
     return 0;
 }
