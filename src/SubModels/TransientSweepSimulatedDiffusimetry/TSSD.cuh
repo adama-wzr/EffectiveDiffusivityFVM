3d4559e5714dd28c42f27e6f0f613dd7596215d9
adama-wzr <adama@ku.edu>
2025-04-30 09:51:54 -0400

    Flux Bug Fix
    - flux was multiplied by dt twice, which was yielding a flux that is much smaller than expected.
    - the specific surface area conversion to real surface area of the physical (3D) system was off, so this parameter has been adjusted.

--- src/SubModels/TransientSweepSimulatedDiffusimetry/TSSD.cuh
+++ src/SubModels/TransientSweepSimulatedDiffusimetry/TSSD.cuh
@@ -454,7 +454,7 @@ void SetBC_TSSD2D(options *opts, TSSDopts *oTSSD, meshInfo *mesh, int *BC, doubl
 
     double volume = 3.1415 * 100e-06 * pow(0.004,2)/4.0;
 
-    flux = mesh->dt * oTSSD->current_density / (mesh->SSA/(pow(oTSSD->pixelRes, 2)) * volume * opts->charge * FARADAY);
+    flux = oTSSD->current_density / (mesh->SSA/(pow(oTSSD->pixelRes, 3)) * volume * opts->charge * FARADAY);
 
     printf("SA: %1.3e m^2, Flux = %1.3e [units?]\n", mesh->SA, flux);
 
