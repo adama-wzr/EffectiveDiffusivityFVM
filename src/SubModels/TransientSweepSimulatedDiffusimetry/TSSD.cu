44a7228a9661e44c8817f0a8f4c1362c22334ead
adama-wzr <adama@ku.edu>
2025-05-02 12:43:18 -0400

    Update TSSD.cu
    - bug fix in GITT data reading

--- src/SubModels/TransientSweepSimulatedDiffusimetry/TSSD.cu
+++ src/SubModels/TransientSweepSimulatedDiffusimetry/TSSD.cu
@@ -224,7 +224,7 @@ int main(int argc, char **argv)
         if(oTSSD.useGITT)
         {
             SOC = mesh.currentTime / oTSSD.totalTime * 100;
-            if (SOC >= GITT_SOC[GITT_idx + 1] && GITT_SOC[GITT_idx] != 0)
+            if (SOC >= GITT_SOC[GITT_idx + 1] && GITT_SOC[GITT_idx] != 0 && GITT_idx < nData)
             {
                 // update DC
                 GITT_idx++;
