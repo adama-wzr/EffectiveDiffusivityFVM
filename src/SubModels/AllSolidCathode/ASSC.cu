571fe7a53d7d2d176229b291edd2ac35f4329f2e
adama-wzr <adama@ku.edu>
2025-06-13 14:47:52 -0400

    arrays
    - title

--- src/SubModels/AllSolidCathode/ASSC.cu
+++ src/SubModels/AllSolidCathode/ASSC.cu
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     ASSCopts oASSC;
 
     // read input data
-    
+
     char inputName[100];
     sprintf(inputName, "inputASSC.txt");
 
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
 
     double growth_factor = 10;
 
-    mesh.dt = growth_factor * pow(mesh.dx, 2)/ oASSC.POI_DC;
+    mesh.dt = growth_factor * pow(mesh.dx, 2) / oASSC.POI_DC;
 
     // calculate AM VF and active surface area
 
@@ -60,6 +60,58 @@ int main(int argc, char **argv)
 
     if (opts.verbose)
         printInputASSC(&opts, &oASSC, &mesh);
+
+    // Declare arrays
+
+    int nDiag;
+
+    if(opts.nD == 2)
+    {
+        nDiag = 5;
+    } else if(opts.nD == 3)
+    {
+        nDiag = 7;
+    }
+
+    double *DC = (double *)malloc(sizeof(double) * mesh.nElements);
+    double *Conc = (double *)malloc(sizeof(double) * mesh.nElements);
+    double *C0 = (double *)malloc(sizeof(double) * mesh.nElements);
+    double *RHS = (double *)malloc(sizeof(double) * mesh.nElements);
+    double *Coeff = (double *)malloc(sizeof(double) * mesh.nElements * nDiag);
+
+    // Intialize arrays
+
+    memset(DC, 0, sizeof(double) * mesh.nElements);
+    memset(Conc, 0, sizeof(double) * mesh.nElements);
+    memset(C0, 0, sizeof(double) * mesh.nElements);
+    memset(RHS, 0, sizeof(double) * mesh.nElements);
+    memset(Coeff, 0, sizeof(double) * mesh.nElements * nDiag);
+    
+    // set DC's
+
+    for (int i = 0; i < mesh.nElements; i++)
+    {
+        if (simData[i] == oASSC.POI)
+        {
+            DC[i] = oASSC.POI_DC;
+        }
+    }
+
+    // initial concentrations
+
+    for (int i = 0; i < mesh.nElements; i++)
+    {
+        if (DC[i] == 0)
+            continue;
+        Conc[i] = oASSC.C0; // mol/m^3
+        C0[i] = oASSC.C0;   // mol/m^3
+    }
+
+    // set BCs
+
+    
+
+    // DC, and BCs
 
     test_funct();
 
